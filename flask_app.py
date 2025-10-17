04b9116754ab13d70052060438dc3ca5897c67f7
boardchat05 <boardchat05@gmail.com>
2025-10-17 23:17:17 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -110,4 +110,4 @@ def index():
     return render_template('index.html', result=result, ai_keys=ai_keys)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)
